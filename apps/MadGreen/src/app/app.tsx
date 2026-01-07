e4f85095375efd8fa5e04fc8467260ab3b04bb56
alberto-delanuez <alberto.delanuez@alten.es>
2026-01-07 15:24:30 +0100

    Initial commit
    feat(nx-cloud): setup nx cloud workspace from react
    
      This commit sets up Nx Cloud for your Nx workspace created from the react, enabling distributed caching and the Nx Cloud GitHub integration for fast CI and improved developer experience.
    
      You can access your Nx Cloud workspace by going to
      https://cloud.nx.app/orgs/695e66401774538391738526/workspaces/695e6ca15d6713612ba0e5e1
    
      **Note:** This commit attempts to maintain formatting of the nx.json file, however you may need to correct formatting by running an nx format command and committing the changes.
    
    completed criteria for module 1
    
    acceptance criteria
    
    starting point, libs are created in the previous step

---
+++ apps/MadGreen/src/app/app.tsx
@@ -0,0 +1,48 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.css';
+
+import { Box } from '@mui/material';
+import { Route, Routes, Link } from 'react-router-dom';
+import { ProfileFeature } from '@onboarding-course/feature-profile';
+
+export function App() {
+  return (
+    <Box component="section" sx={{ p: 2, border: '1px solid green' }}>
+
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/page-2">Page 2</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              This is the generated root route.{' '}
+              <Link to="/page-2">Click here for page 2.</Link>
+            </div>
+          }
+        />
+        <Route
+          path="/page-2"
+          element={
+            <div>
+              <Link to="/">Click here to go back to root page.</Link>
+            </div>
+          }
+        />
+      </Routes>
+      {/* END: routes */}
+      <ProfileFeature /> 
+    </Box>
+    
+  );
+}
+
+export default App;
---
+++ libs/auth/feature/src/lib/feature.tsx
@@ -0,0 +1,11 @@
+import styles from './feature.module.css';
+
+export function OnboardingCourseFeature() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to OnboardingCourseFeature!</h1>
+    </div>
+  );
+}
+
+export default OnboardingCourseFeature;
