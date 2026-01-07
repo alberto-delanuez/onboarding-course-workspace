bf7805b62fb5980abe10549ae199117883ee1605
alberto-delanuez <alberto.delanuez@alten.es>
2026-01-07 15:24:30 +0100

    Initial commit
    feat(nx-cloud): setup nx cloud workspace from react
    
      This commit sets up Nx Cloud for your Nx workspace created from the react, enabling distributed caching and the Nx Cloud GitHub integration for fast CI and improved developer experience.
    
      You can access your Nx Cloud workspace by going to
      https://cloud.nx.app/orgs/695e66401774538391738526/workspaces/695e6ca15d6713612ba0e5e1
    
      **Note:** This commit attempts to maintain formatting of the nx.json file, however you may need to correct formatting by running an nx format command and committing the changes.
    
    completed criteria for module 1
    
    acceptance criteria
    
    starting point, libs are created in the previous step

---
+++ libs/profile/data/src/lib/data.tsx
@@ -0,0 +1,11 @@
+import styles from './data.module.css';
+
+export function OnboardingCourseData() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to OnboardingCourseData!</h1>
+    </div>
+  );
+}
+
+export default OnboardingCourseData;
