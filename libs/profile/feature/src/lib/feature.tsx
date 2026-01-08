21908ee13f2a7638e150a097a5cd05c51ba94c31
alberto-delanuez <alberto.delanuez@alten.es>
2026-01-08 08:22:53 +0100

    completed criteria for module 1

---
+++ libs/profile/feature/src/lib/feature.tsx
@@ -0,0 +1,11 @@
+import styles from './feature.module.css';
+
+export function ProfileFeature() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to ProfileFeature!</h1>
+    </div>
+  );
+}
+
+export default ProfileFeature;
