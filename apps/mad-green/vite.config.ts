a7501cb67a9c8271e156b0970339920c30b20d5a
alberto-delanuez <alberto.delanuez@alten.es>
2026-01-08 10:21:25 +0100

    new starting point

---
+++ apps/mad-green/vite.config.ts
@@ -0,0 +1,37 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => ({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/mad-green',
+  server: {
+    port: 4201,
+    host: 'localhost',
+  },
+  preview: {
+    port: 4201,
+    host: 'localhost',
+  },
+  plugins: [react()],
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  test: {
+    name: '@onboarding-course/mad-green',
+    watch: false,
+    globals: true,
+    environment: 'jsdom',
+    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: './test-output/vitest/coverage',
+      provider: 'v8' as const,
+    },
+  },
+}));
