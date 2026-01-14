d53b06c6a00d4bdee317ff6cfcecfeeb62bdd6eb
alberto-delanuez <alberto.delanuez@alten.es>
2026-01-14 15:13:53 +0100

    no problems in workspace

--- libs/customer/profile/domain/src/index.ts
+++ libs/customer/profile/domain/src/index.ts
@@ -1,5 +1,3 @@
 export * from './lib/models/user-profile.model';
 export * from './lib/dtos/update-profile.dto';
 export * from './lib/repositories/profile.repository';
-export * from './lib/utils/address.utils';
-export * from './lib/utils/name.utils';
