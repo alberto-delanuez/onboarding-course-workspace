cdc349e8a4d3946a2ccad2d0fdd1e01c57090fd6
alberto-delanuez <alberto.delanuez@alten.es>
2026-01-15 13:02:36 +0100

    adding test in common/ui

---
+++ libs/customer/common/ui/src/lib/widgets/contracts-widget/contracts-widget.spec.tsx
@@ -0,0 +1,25 @@
+import '@testing-library/jest-dom';
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { IntlProvider } from 'react-intl';
+import { ContractsWidget } from './contracts-widget';
+
+const Wrapper = ({ children }: { children: React.ReactNode }) => (
+  <IntlProvider locale="en">{children}</IntlProvider>
+);
+
+describe('ContractsWidget', () => {
+  it('renders title and list of contracts', () => {
+    render(
+      <Wrapper>
+        <ContractsWidget />
+      </Wrapper>
+    );
+
+    expect(screen.getByText('My Contracts')).toBeInTheDocument();
+
+    expect(screen.getByText('Go Max Cinema')).toBeInTheDocument();
+    expect(screen.getByText('Home Fiber 1Gbps')).toBeInTheDocument();
+    expect(screen.getByText('Family TV Pack')).toBeInTheDocument();
+  });
+});
